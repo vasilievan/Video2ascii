124c39796a534ddd0e9cd67c34a29929cdae5ca2
aleksey <vasiliev.an@edu.spbstu.ru>
2022-07-07 10:07:16 +0300

    Front/Back camera implemented via click

--- app/src/main/java/aleksey/vasilev/video2ascii/MainActivity.kt
+++ app/src/main/java/aleksey/vasilev/video2ascii/MainActivity.kt
@@ -9,13 +9,13 @@ import androidx.activity.compose.setContent
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -23,13 +23,22 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.Executor
 
 class MainActivity : ComponentActivity() {
     private lateinit var analyzer: ImageAnalyzer
+    private lateinit var buildImageAnalysis: ImageAnalysis
+    private lateinit var imageView: ImageView
+    private val imageAnalysis = ImageAnalysis.Builder().apply {
+        setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+        setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+    }
 
     @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,33 +79,45 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnsafeOptInUsageError")
     @Composable
     private fun VideoPreview(modifier: Modifier) {
-        val lifecycleOwner = LocalLifecycleOwner.current
         val currentContext = LocalContext.current
+        val lifecycleOwner = LocalLifecycleOwner.current
         val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(currentContext) }
         val executor = ContextCompat.getMainExecutor(currentContext)
-        AndroidView(modifier = modifier, factory = { context ->
-            val imageView = ImageView(context)
+        var direction by remember {
+            mutableStateOf(1)
+        }
+        AndroidView(modifier = modifier.clickable {
+            direction = (direction + 1) % 2
+            addListener(cameraProviderFuture, direction, lifecycleOwner, executor)
+            imageView.rotation = if (direction == 0) 180f else 0f
+        }, factory = { context ->
+            imageView = ImageView(context)
             analyzer = ImageAnalyzer(imageView)
-            val imageAnalysis = ImageAnalysis.Builder().apply {
-                setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-            }
-            val buildImageAnalysis = imageAnalysis.build().apply {
+            buildImageAnalysis = imageAnalysis.build().apply {
                 setAnalyzer(executor, analyzer)
             }
-            cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
-                val cameraSelector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    cameraSelector,
-                    buildImageAnalysis,
-                )
-            }, executor)
+            addListener(cameraProviderFuture, direction, lifecycleOwner, executor)
             imageView
         })
+    }
+
+    private fun addListener(
+        cameraProviderFuture: ListenableFuture<ProcessCameraProvider>,
+        direction: Int,
+        lifecycleOwner: LifecycleOwner,
+        executor: Executor
+    ) {
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(direction)
+                .build()
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                lifecycleOwner,
+                cameraSelector,
+                buildImageAnalysis,
+            )
+        }, executor)
     }
 }
